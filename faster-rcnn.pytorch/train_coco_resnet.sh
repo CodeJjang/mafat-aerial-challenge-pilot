d913e42c275d634e10157afac27fa00630a8d597
aviad <aviad10m@gmail.com>
2018-03-06 00:27:10 +0200

    Added faster rcnn, working on pascal and coco

---
+++ faster-rcnn.pytorch/train_coco_resnet.sh
@@ -0,0 +1,5 @@
+python trainval_net.py --dataset coco --net res101 \
+                       --bs 8 --nw 2 \
+                       --lr 4e-3 --lr_decay_step 8 --epochs 2 \
+                       --cuda --mGPUs \
+                       --save_dir saved_models
---
+++ switch_env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+bash
+source activate ddrnd_project
