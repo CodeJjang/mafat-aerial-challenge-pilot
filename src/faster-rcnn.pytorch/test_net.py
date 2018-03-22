33d0b5d9918dc095b8594e0168506bfcc2b2271b
Aviad Moreshet <aviad10m@gmail.com>
2018-03-22 08:43:02 +0200

    Changed ddrnd anchor scales

--- src/faster-rcnn.pytorch/test_net.py
+++ src/faster-rcnn.pytorch/test_net.py
@@ -130,7 +130,7 @@ if __name__ == '__main__':
   elif args.dataset == 'ddrnd':
       args.imdb_name = "voc_2007_trainval"
       args.imdbval_name = "voc_2007_test"
-      args.set_cfgs = ['ANCHOR_SCALES', '[8, 16, 32]', 'ANCHOR_RATIOS', '[0.5,1,2]']
+      args.set_cfgs = ['ANCHOR_SCALES', '[1, 2, 4, 8, 16]', 'ANCHOR_RATIOS', '[0.5,1,2]']
 
   args.cfg_file = "cfgs/{}_ls.yml".format(args.net) if args.large_scale else "cfgs/{}.yml".format(args.net)
 
